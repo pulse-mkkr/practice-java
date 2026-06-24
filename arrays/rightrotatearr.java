839af4f17624b3fae91be31e63d1a473f73974ab
Manish <manishkkr071083@gmail.com>
2026-06-24 22:18:29 +0530

    Create rightrotatearr.java

---
+++ arrays/rightrotatearr.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class arrrightrotate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any n :");
+        int n = sc.nextInt();
+        sc.close();
+        int arr[]={1,2,3,4,5,6,7,8,9};
+        int al=arr.length,k=n%al;
+        int ans[]=new int[al];
+        if(k==0){
+            System.out.println("remains same");
+            return;
+        }
+        
+        for(int i=0;i<k;i++){
+            ans[i]=arr[al-k+i];
+        }
+        for(int i=0;i<al-k;i++){
+            ans[k+i]=arr[i];
+        }
+        for(int i=0;i<al;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+}
